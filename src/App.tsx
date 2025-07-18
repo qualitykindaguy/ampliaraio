ab5d6ac1d4e719e603e028f1716944720028ebc0
Hugh Billings <120135330+qualitykindaguy@users.noreply.github.com>
2025-07-18 18:12:21 -0400

    Create Fundable Founders landing page with embed form

--- src/App.tsx
+++ src/App.tsx
@@ -13,6 +13,7 @@ import About from './pages/About';
 import Contact from './pages/Contact';
 import LiveAmplified from './pages/LiveAmplified';
 import ShopAmplified from './pages/ShopAmplified';
+import FundableFounders from './pages/FundableFounders';
 
 function App() {
   return (
@@ -30,6 +31,7 @@ function App() {
       <Route path="/contact" element={<Contact />} />
       <Route path="/liveamplified" element={<LiveAmplified />} />
       <Route path="/shopamplified" element={<ShopAmplified />} />
+      <Route path="/fundable-founders" element={<FundableFounders />} />
     </Routes>
   );
 }
