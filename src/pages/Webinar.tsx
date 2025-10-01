e4fee3ed773d596083ae12ebe7355b2e5969f340
Hugh Billings <120135330+qualitykindaguy@users.noreply.github.com>
2025-10-01 15:10:04 -0400

    Updated Webinar.tsx

--- src/pages/Webinar.tsx
+++ src/pages/Webinar.tsx
@@ -196,3 +196,6 @@ function Webinar() {
             <div>
               <h3 className="text-2xl md:text-3xl font-black text-[#996516] mb-2">Sainte Billings, CPA</h3>
               <p className="text-lg md:text-xl font-bold text-gray-700">
+
+  )
+}
